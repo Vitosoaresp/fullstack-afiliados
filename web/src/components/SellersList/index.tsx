ede3d48ec071f12f690a0e85d1be6ced503c4157
vitor <vitor.soares@hox.rs>
2023-07-24 13:53:52 -0300

    feat[front]: add pagination on  table for sellers

--- web/src/components/SellersList/index.tsx
+++ web/src/components/SellersList/index.tsx
@@ -1,4 +1,13 @@
-import { Paper, Table, TableBody, TableContainer } from '@mui/material';
+import {
+	Paper,
+	Table,
+	TableBody,
+	TableContainer,
+	TableFooter,
+	TablePagination,
+	TableRow,
+} from '@mui/material';
+import { useState } from 'react';
 import useSWR from 'swr';
 import { Seller } from '../../types/seller';
 import SpinLoading from '../SpinLoading';
@@ -7,23 +16,50 @@ import { Row } from './TableRow';
 
 export function SellersList() {
 	const { data, isLoading } = useSWR<Seller[]>('/sellers');
-
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
 	const hasData = !isLoading && data && data.length > 0;
+
+	const handleChangePage = (_event: unknown, newPage: number) => {
+		setPage(newPage);
+	};
+
+	const handleChangeRowsPerPage = (
+		event: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		setRowsPerPage(+event.target.value);
+		setPage(0);
+	};
 	return (
 		<TableContainer component={Paper} variant='elevation'>
 			<Table>
 				<TableHeader />
 				<TableBody>
 					{hasData &&
-						data?.map((seller) => (
-							<Row
-								key={seller.id}
-								name={seller.name}
-								transactions={seller.Transaction}
-								type={seller.type}
-							/>
-						))}
+						data
+							.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+							.map((seller) => (
+								<Row
+									key={seller.id}
+									name={seller.name}
+									transactions={seller.Transaction}
+									type={seller.type}
+								/>
+							))}
 				</TableBody>
+				<TableFooter>
+					<TableRow>
+						<TablePagination
+							rowsPerPageOptions={[5, 10, 20]}
+							labelRowsPerPage='Linhas por página'
+							count={data?.length || 0}
+							rowsPerPage={rowsPerPage}
+							page={page}
+							onPageChange={handleChangePage}
+							onRowsPerPageChange={handleChangeRowsPerPage}
+						/>
+					</TableRow>
+				</TableFooter>
 			</Table>
 			{isLoading && <SpinLoading />}
 		</TableContainer>
