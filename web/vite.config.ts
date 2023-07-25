57a3c6661c98d4de3fddfba255ce2cae65aa7512
vitor <vitor.soares@hox.rs>
2023-07-24 21:13:35 -0300

    [frontend]: setup for tests

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,21 @@
+/// <reference types="vitest" />:
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './setupTests.ts',
+		css: true,
+		reporters: ['verbose'],
+		coverage: {
+			reporter: ['text', 'json', 'html'],
+			provider: 'c8',
+		},
+	},
 	server: {
 		host: true,
 		port: 3000,
